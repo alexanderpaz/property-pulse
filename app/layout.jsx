31bc51014cf6ecbcc2afdb8f4dd13e864d0e18d8
Alex Paz <alexanderpaz@gmail.com>
2024-03-30 10:48:16 -0500

    AuthProvider implementado y embebido en el layout. Env actualizado con NEXTAUTH parameters. Se generó el secret con openssl rand -base64 32

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,34 +1,36 @@
-import '@/assets/styles/global.css';
+import "@/assets/styles/global.css";
 
-import Navbar from '@/components/Navbar';
-import Footer from '@/components/Footer';
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+import AuthProvider from "@/components/AuthProvider";
 
 export const viewport = {
-    width: 'device-width',
-    initialScale: 1.0,
+  width: "device-width",
+  initialScale: 1.0,
 };
 
 export const metadata = {
-    charSet: 'UTF-8',
-    title: 'PropertyPulse | Find The Perfect Rental',
-    description: 'Find your dream rental property',
-    keywords: 'rental, find rentals, find properties',
+  charSet: "UTF-8",
+  title: "PropertyPulse | Find The Perfect Rental",
+  description: "Find your dream rental property",
+  keywords: "rental, find rentals, find properties",
 };
 
 const MainLayout = ({ children }) => {
-    return (
-        <html lang="en">
+  return (
+    <AuthProvider>
+      <html lang="en">
         <body>
-        <header>
+          <header>
             <Navbar />
-        </header>
-        <main>
-            {children}
-        </main>
-        <Footer />
+          </header>
+          <main>{children}</main>
+          <Footer />
         </body>
-        </html>
+      </html>
+    </AuthProvider>
+  );
+};
 
-)}
-
-export default MainLayout
+export default MainLayout;

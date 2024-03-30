a962fb721b4309af5db098772b4111e9467896cf
Alex Paz <alexanderpaz@gmail.com>
2024-03-30 10:24:52 -0500

    Property Details implementado para datos principales. Se agregaron credenciales OAuth

--- utils/requests.js
+++ utils/requests.js
@@ -1,14 +1,17 @@
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
 
+//Fetch all properties
+
 async function fetchProperties() {
   try {
 
     // Handle the case where the domain is not available yet
     if (!apiDomain) {
+      log.console('Domain is not available yet returning empty list')
       return [];
     }
 
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_DOMAIN}/properties`);
+    const res = await fetch(`${apiDomain}/properties`);
 
     if (!res.ok) {
       throw new Error("Failed to fetch properties");
@@ -20,4 +23,27 @@ async function fetchProperties() {
   }
 }
 
-export { fetchProperties };
+// Fetch single property
+
+async function fetchProperty(id) {
+  try {
+
+    // Handle the case where the domain is not available yet
+    if (!apiDomain) {
+      log.console('Domain is not available yet returning empty list')
+      return null;
+    }
+
+    const res = await fetch(`${apiDomain}/properties/${id}`);
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch properties");
+    }
+    return res.json();
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+export { fetchProperties, fetchProperty };

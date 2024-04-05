a53394869c09f9a25c405a51f1086cd3bf105013
Alex Paz <alexanderpaz@gmail.com>
2024-04-05 14:42:46 -0500

    Messages Page shows messages contents

--- app/api/messages/route.js
+++ app/api/messages/route.js
@@ -1,60 +1,103 @@
 import connectDB from "@/config/database";
 import Message from "@/models/Message";
-import { getSessionUser } from '@/utils/getSessionUser';
+import { getSessionUser } from "@/utils/getSessionUser";
 
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
+
+// GET /api/messages
+export const GET = async (request) => {
+  try {
+    await connectDB();
+
+    const sessionUser = await getSessionUser();
+
+    if (!sessionUser || !sessionUser.user) {
+      return new Response(
+        JSON.stringify({
+          message: "Usert ID is required",
+        }),
+        {
+          status: 401,
+        }
+      );
+    }
+
+    const { userId } = sessionUser;
+
+    const messages = await Message.find({ recipient: userId })
+      .populate("sender", "username")
+      .populate("property", "name");
+
+    return new Response(JSON.stringify(messages), { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return new Response("Something went wrong", {
+      status: 500,
+    });
+  }
+};
 
 // POST /api/messages
 
 export const POST = async (request) => {
-    try {
-        await connectDB();
+  try {
+    await connectDB();
 
-        const { name, email, phone, message, property, recipient } = await request.json();
+    const { name, email, phone, message, property, recipient } =
+      await request.json();
 
-        const sessionUser = await getSessionUser();
+    const sessionUser = await getSessionUser();
 
-        if (!sessionUser || !sessionUser.user ) {
-            return new Response(JSON.stringify({
-                message: 'You must be logged in to send a message' }), {
-                status: 401
-            });
+    if (!sessionUser || !sessionUser.user) {
+      return new Response(
+        JSON.stringify({
+          message: "You must be logged in to send a message",
+        }),
+        {
+          status: 401,
         }
-
-        const { user } = sessionUser;
-
-        // Can not send message to self
-
-        if (user.id === recipient) {
-            return new Response(JSON.stringify({
-                message: 'Can not send message to self' }), {
-                status: 400 
-            });
-        }
-
-        const newMessage = new Message({
-            sender: user.id,
-            recipient,
-            property,
-            name,
-            email,
-            phone,
-            body: message
-        });
-
-        await newMessage.save();
-
-        return new Response(JSON.stringify({
-            message: 'Message sent successfully'
-        }), {
-            status: 200
-        });
-
-    } catch (error) {
-        console.log(error);
-        return new Response('Something went wrong', {
-            status: 200
-        });
-        
+      );
     }
+
+    const { user } = sessionUser;
+
+    // Can not send message to self
+
+    if (user.id === recipient) {
+      return new Response(
+        JSON.stringify({
+          message: "Can not send message to self",
+        }),
+        {
+          status: 400,
+        }
+      );
+    }
+
+    const newMessage = new Message({
+      sender: user.id,
+      recipient,
+      property,
+      name,
+      email,
+      phone,
+      body: message,
+    });
+
+    await newMessage.save();
+
+    return new Response(
+      JSON.stringify({
+        message: "Message sent successfully",
+      }),
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    return new Response("Something went wrong", {
+      status: 200,
+    });
+  }
 };
